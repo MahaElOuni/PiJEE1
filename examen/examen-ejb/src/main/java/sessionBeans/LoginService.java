d7ad5fcfc1b3da102ee6266e8ecc9591e7e5c369
khouloud sma <khouloud.sma@esprit.tn>
2019-04-25 10:22:17 +0100

    sala7et

--- examen/examen-ejb/src/main/java/sessionBeans/LoginService.java
+++ examen/examen-ejb/src/main/java/sessionBeans/LoginService.java
@@ -2,6 +2,9 @@ package sessionBeans;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
 /**
  * Session Bean implementation class LoginService
@@ -16,5 +19,14 @@ public class LoginService implements LoginServiceRemote, LoginServiceLocal {
     public LoginService() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public void Login() {
+		// TODO Auto-generated method stub
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("");
+		WebTarget login=target.path("");
+		
+	}
 
 }
